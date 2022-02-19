f9bb371053fced6ce5a5db43c9dad983e98d1d0c
Fredrik Svahn <fredriksvahn97@gmail.com>
2022-02-19 23:53:14 +0100

    fresh start

---
+++ project-manager-frontend/storage/framework/views/84e0741d078d93e094a98716fb43b3349040034f.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <title><?php echo $__env->yieldContent('title'); ?></title>
+</head>
+<body>
+    <div class="col-md-12">
+        <?php if($errors->any()): ?>
+        <div class="alert-danger">
+            Errors:
+            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <?php echo e($error); ?>
+
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </div>
+        <?php endif; ?>
+
+        <?php echo $__env->yieldContent('body'); ?>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
+<?php /**PATH C:\Users\Fredde\Documents\GitHub\project-manager\project-manager-frontend\resources\views/head.blade.php ENDPATH**/ ?>
---
+++ project-manager-frontend/storage/framework/views/b68eaf9c7748167402f2f67d9c53a172d61e9a17.php
@@ -0,0 +1,14 @@
+<?php $__env->startSection('body'); ?>
+    <h1>Your projects</h1>
+    <table class="table">
+        <tr>
+            <th>Name</th>
+            <th>ID</th>
+            <th>Contact</th>
+            <th>Department</th>
+            <th></th>
+        </tr>
+    </table>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('head', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Fredde\Documents\GitHub\project-manager\project-manager-frontend\resources\views/user/dashboard.blade.php ENDPATH**/ ?>
